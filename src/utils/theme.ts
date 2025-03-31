20da6a77ac00bb5923d48c769021065784a4fa38
luizavalentini <luizadaravalentini@gmail.com>
2025-03-31 10:08:45 -0300

    fix: app colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,44 +2,168 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: "#c1a047",
+    customBackground: {
+      primary: "#EEEEEE",
+      secondary: "#F7F7F7",
+      gray: "#8D8D8D",
+      black: "#000000",
     },
-    warning: {
-      main: "#f23030",
+    customButton: {
+      primary: "#D9D9D9",
+      white: "#FFFFFF",
+      black: "#D9D9D9",
+      gray: "#4E4E4E",
     },
-    background: {
-      paper: "#1c1c1c",
-      default: "#181818",
+    customText: {
+      black: "#D9D9D9",
+      gray: "#565656",
+      lightGrey: "#8D8D8D",
     },
-    text: {
-      primary: "#ffffff",
-      secondary: "#c1a047",
+    customInput: {
+      primary: "#E3E3E3",
+      white: "#FFFFFF",
+      gray: "#A0AEC0",
+      darkGrey: "#8D8D8D",
+      lightGrey: "#D1D1D1",
+      veryLightGray: "#EEEEEE",
     },
-    custom: {
-      gold: "#c1a047",
+    icon: {
+      black: "#000000",
+      white: "#FFFFFF",
     },
-    input: {
-      primary: "",
+    chart: {
+      darkBrown: "#4A4331",
+      reddishBrow: "#D6CFBF",
+      oliveBrown: "#624F1C",
+      mustardBrown: "#D6CFBF",
+      goldenYellow: "#C1A047",
+      lightBeige: "#D6CFBF",
+      lightGray: "#D6CFBF",
+    },
+    hover: {
+      primary: "#F5F5F5",
+    },
+    table: {
+      primary: "#D9D9D9",
+      white: "#FFFFFF",
+    },
+    suspectTable: {
+      paleBeige: "#EFE5C9",
+      mediumGray: "#F1F1F5",
+      gold: "#C1A047",
+      black: "#000000",
+      white: "#F1F1F5",
     },
   },
 });
 
 declare module "@mui/material/styles" {
   interface Palette {
-    custom: {
-      gold: string;
-    };
-    input: {
+    customBackground: {
       primary: string;
+      secondary: string;
+      gray: string;
+      black: string;
+    };
+    customButton: {
+      primary: string;
+      white: string;
+      black: string;
+      gray: string;
+    };
+    customText: {
+      black: string;
+      gray: string;
+      lightGrey: string;
+    };
+    customInput: {
+      primary: string;
+      white: string;
+      gray: string;
+      darkGrey: string;
+      lightGrey: string;
+      veryLightGray: string;
+    };
+    icon: {
+      black: string;
+      white: string;
+    };
+    suspectTable: {
+      paleBeige: string;
+      mediumGray: string;
+      gold: string;
+      black: string;
+      white: string;
+    };
+    chart: {
+      darkBrown: string;
+      reddishBrow: string;
+      oliveBrown: string;
+      mustardBrown: string;
+      goldenYellow: string;
+      lightBeige: string;
+      lightGray: string;
+    };
+    hover: {
+      primary: string;
+    };
+    table: {
+      primary: string;
+      white: string;
     };
   }
   interface PaletteOptions {
-    custom?: {
-      gold?: string;
+    customBackground?: {
+      primary?: string;
+      secondary?: string;
+      gray?: string;
+      black?: string;
     };
-    input: {
+    customButton: {
       primary: string;
+      white: string;
+      black: string;
+      gray: string;
+    };
+    customText: {
+      black: string;
+      gray: string;
+      lightGrey: string;
+    };
+    customInput: {
+      primary: string;
+      white: string;
+      gray: string;
+      darkGrey: string;
+      lightGrey: string;
+      veryLightGray: string;
+    };
+    icon: {
+      black: string;
+      white: string;
+    };
+    chart: {
+      darkBrown: string;
+      reddishBrow: string;
+      oliveBrown: string;
+      mustardBrown: string;
+      goldenYellow: string;
+      lightBeige: string;
+      lightGray: string;
+    };
+    hover: {
+      primary: string;
+    };
+    table: {
+      primary: string;
+      white: string;
+    };
+    suspectTable: {
+      paleBeige: string;
+      mediumGray: string;
+      gold: string;
+      black: string;
+      white: string;
     };
   }
 }
