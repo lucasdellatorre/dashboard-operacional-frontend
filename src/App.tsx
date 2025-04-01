28f1479d85b8a581e7fc0a74fc0f90331bc2af7c
luizavalentini <luizadaravalentini@gmail.com>
2025-04-01 20:15:41 -0300

    feature: target page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Layout from "./components/layout/layout";
 import Dashboard from "./routes/dashboard";
 import Login from "./routes/login";
 import Users from "./routes/users";
+import Suspects from "./routes/suspects";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Dashboard />} />
+        <Route index path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
+        <Route path="/alvos" element={<Suspects />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
