9e61cbbac3462c88eed386330d89f57b8f010bad
leonardo.monteiro <leonardo.monteiro004@edu.pucrs.br>
2025-04-06 13:29:27 -0300

    feature: criação da tela de operacao, componente de operacao e o filtro deles.

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Dashboard from "./routes/dashboard";
 import Login from "./routes/login";
 import Users from "./routes/users";
 import Suspects from "./routes/suspects";
+import Operation from "./routes/operation";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index path="/dashboard" element={<Dashboard />} />
+        <Route index path="/operation" element={<Operation />} />
         <Route path="/users" element={<Users />} />
         <Route path="/alvos" element={<Suspects />} />
       </Route>
