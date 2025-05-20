95a19bce66890605815c98a075f01b905adeda04
Lucas Dellatorre <lucas.dellatorre@edu.pucrs.br>
2025-05-20 15:56:07 -0300

    hotfix: remove /api on server

--- src/server/service.ts
+++ src/server/service.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://dashboard-operacional-backend-4b392a9c5ff4.herokuapp.com/api",
+  baseURL: "https://dashboard-operacional-backend-4b392a9c5ff4.herokuapp.com",
 });
