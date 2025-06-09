1cf22c3ca178eb26c2ffa1d28809103cab8a7a78
luizavalentini <luizadaravalentini@gmail.com>
2025-06-08 21:31:45 -0300

    add: test

--- cypress/e2e/createSuspectTest.cy.js
+++ cypress/e2e/createSuspectTest.cy.js
@@ -21,21 +21,20 @@ describe("CreateSuspectModal", () => {
     ).click();
     cy.contains("(54) 997088840").click();
     cy.contains("(51) 98394938").click();
-    cy.get("body").click(0, 0); // Click outside to close dropdown
 
-    // Select operations
-    cy.contains("Selecione as operações").click();
-    cy.contains("li", "Operação A").click();
-    cy.contains("li", "Operação B").click();
-    cy.get("body").click(0, 0); // Click outside to close dropdown
+    cy.get('input[placeholder="Digite o CPF do suspeito"]')
+      .clear()
+      .type("12345678901");
+
+    cy.get('input[placeholder="Digite o CPF do suspeito"]')
+      .clear()
+      .type("05102625039");
 
     // Click "Criar suspeito" button
-    cy.contains("Criar suspeito").click();
+    cy.contains("Criar alvo").click();
+    // Click outside to close dropdown
 
     // Expect modal to close (or assert something on submit)
     cy.contains("Criação Alvos").should("not.exist");
-
-    // Optional: assert side effect like a new row added to a table
-    // cy.contains('Suspeito Teste').should('exist');
   });
 });
